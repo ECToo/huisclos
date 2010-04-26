0a99927a194d938a450efe72c8c3da861c3f50f6
AngeliqeR <AngeliqeR@e83817fc-7a63-49ad-8ecc-3c91c5b9472f>
2010-04-26 03:48:20 +0000

    Cleanup and implementation of acceleration. Still need to fix steering force to have proper physics.

--- Agent.hpp
+++ Agent.hpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <sys/time.h>
 #include "SteeringBehavior.hpp"
-#include "AIBrain.hpp"
 #include "Wall.hpp"
 
 using namespace irr;
@@ -31,7 +30,6 @@ class Agent
    public:
 		static s32 genID();
 		Agent(IrrlichtDevice* d, stringw mesh, stringw t, stringw h, vector3df p);
-		Agent(IrrlichtDevice* d, stringw mesh, stringw t, stringw h, vector3df p, AIBrain b);
 		virtual ~Agent();
 		vector<f32> DrawFeelers(bool debug = false);  //wall collision detection
       vector<pointOfInterest> DrawCircle(const vector<Agent*>& agents, bool debug = false);  //shows circular range and agents in range
@@ -44,11 +42,7 @@ class Agent
 		IAnimatedMeshSceneNode* getBody(void);  //return the scene node
 		void Update(void);  //sense,think,act
 		void Seek(vector3df goal, Wall *w, bool track = false);  //go to a point
-		u32 GetFitness(vector3df goal, vector3df startpos);  //score for reaching goal
 		void GameOver(void);
-		AIBrain GetBrain(void);  //for baby making
-		void Reset(vector3df p, AIBrain b);
-		void SmartNavigate(void);
 
    private:
 		static s32 nextAvailableID;
@@ -65,12 +59,9 @@ class Agent
       stringw texture;  //path+file of the body's texture
       SteeringBehavior wheel;  //for seek function
       std::list<vector3df> route;  //stack of subgoals
-      AIBrain brain;  //neural network
       timespec gstart;  //start time of goal
       timespec gend;  //end time, found goal
-      bool won;  //goal is reached
       bool remove;
-      int stuck;
 
       vector<f32> Sense(void);  //look for walls
       void Think(vector<f32> feelers); //strategize for walls
